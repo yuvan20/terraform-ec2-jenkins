af15a9fe839db6c4464e931cd7a2909991c7e490
yuvan20 <115212455+yuvan20@users.noreply.github.com>
2026-07-26 01:19:30 +0530

    Update backend.tf

--- terraform-ec2-jenkins/terraform-ec2-jenkins/backend.tf
+++ terraform-ec2-jenkins/terraform-ec2-jenkins/backend.tf
@@ -1,1 +1,8 @@
-terraform{backend "s3"{bucket="terraform-state-demouu" key="ec2/terraform.tfstate" region="ap-south-1" dynamodb_table="terraform-lock"}}
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-demouu"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
