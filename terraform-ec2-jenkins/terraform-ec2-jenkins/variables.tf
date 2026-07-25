082aab043aba743d5f80397cf6bb5df6c8b43419
yuvan20 <115212455+yuvan20@users.noreply.github.com>
2026-07-26 01:20:21 +0530

    Update variables.tf

--- terraform-ec2-jenkins/terraform-ec2-jenkins/variables.tf
+++ terraform-ec2-jenkins/terraform-ec2-jenkins/variables.tf
@@ -1,4 +1,19 @@
-variable "region"{}
-variable "ami"{}
-variable "instance_type"{}
-variable "instance_name"{}
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 Instance Name"
+  type        = string
+}
