4a419a518720748ea281c6ec760ce983b2c32b5c
yuvan20 <115212455+yuvan20@users.noreply.github.com>
2026-07-26 01:17:23 +0530

    Update main.tf

--- terraform-ec2-jenkins/terraform-ec2-jenkins/main.tf
+++ terraform-ec2-jenkins/terraform-ec2-jenkins/main.tf
@@ -1,6 +1,7 @@
 module "ec2" {
- source="./modules/ec2"
- ami=var.ami
- instance_type=var.instance_type
- instance_name=var.instance_name
+  source = "./modules/ec2"
+
+  ami            = var.ami
+  instance_type  = var.instance_type
+  instance_name  = var.instance_name
 }
